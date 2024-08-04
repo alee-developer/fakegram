2c76e865ca9700d4c2e67bd4c3f903aebec27fd2
ansar <ansarali.edugaon@gmail.com>
2024-08-04 17:43:41 +0530

    configuration done

---
+++ lib/views/utils/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xfff6ac3a);
+// const kPrimaryColor = Color(0xFF6F35A5);
+const kPrimaryLightColor = Color(0xfff6ac3a);
+const kPrimaryLightColor_gree
+= Color(0xFFA8EA89);
+
+const colorBlack = Colors.black;
+const colorWhite = Colors.white;
+const colorGreen = Colors.green;
+
+//colors
+const primaryColor = Color(0xfff6ac3a);
+const secondaryColor =Color(0xFFf4b44f);
+const dotColor = Color(0xFFf4b44f);
+const bgColor = Color(0xfff6ac3a);
---
+++ lib/views/utils/constants.dart
@@ -0,0 +1,56 @@
+/// keys
+const loginKey = "login";
+const DATABASE_NAME = "got.db";
+const DATABASE_VERSION = 1;
+
+/// app values
+const QC_LOT_NO = "QC Lot No";
+const QC_NO = "QC no";
+const CROP = "Crop";
+const CROP_NAME = "Crop Name";
+const SELECT_CROP = "Select Crop";
+const ITEM_CODE = "Item Code";
+const DATE_OF_PLANTING = "Date of planting";
+const GROWER_CODE = "Grower Code";
+const GROWER_NAME = "Grower Name";
+const GROWER_ADDRESS = "Grower Address";
+const DATE_OF_EVALUATION = "Date of evaluation";
+const TOTAL_NO_OF_PLANTS = "Total no of plants";
+const TOTAL_NO_OF_OFFTYPES = "Total no of off types";
+const NO_OF_OFFTYPE = "No of type";
+const TOTAL_NO_OF_ADMIXTURES = "Total no of admixtures";
+const GENERIC_PURITY = "Generic purity";
+const EVALUATION_BY = "Evaluation by";
+const RESULT = "Result";
+const PASS = "Pass";
+const FAIL = "Fail";
+const STATUS = "Status";
+const FEMALE = "Female";
+const MALE = "Male";
+const OTHERS = "Others";
+const FEMALE_PLANT = "Female Plants";
+const MALE_PLANTS = "Male Plants";
+const A_TYPE_PLANTS = "A type plants";
+const B_TYPE_PLANTS = "B type plants";
+const MALE_TYPE_PLANTS = "Male type plants";
+const TALL_FERTILE_PLANTS = "Tall Fertile Plants";
+const TALL_STERILE_PLANTS = "Tall Sterile Plants";
+const OFFTYPE_FERTILE_PLANTS = "OffType Fertile Plants";
+const OFFTYPE_STERILE_PLANTS = "OffType Sterile Plants";
+
+const SUBMIT = "Submit";
+const NEXT = "Next";
+const DONE = "Done";
+const FROM = "From";
+const SURE  = "Sure";
+const NOT_NOW = "Not Now";
+const TO = "To";
+const CHARACTERS = "Characters";
+const STANDARD = "Standard";
+const OBSERVED = "Observed";
+const DUS_CHARACTERISTICS = "DUS Characteristics";
+const GOT_EVALUATION = "GOT Evaluation";
+const OBSERVATION = "Observation";
+
+const LOGOUT = "Logout";
+const LOGOUT_MESSAGE = "Are you sure? Do you want to logout now?";
